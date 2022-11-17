79b27ef1c155a55ac4022e5694e3d17f538793a0
Kush Agarwal <agarwalkushajay@gmail.com>
2022-11-18 00:48:04 +0530

    Added Command to Create Table

--- src/task/DBConnect.java
+++ src/task/DBConnect.java
@@ -4,6 +4,7 @@ package task;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 
  /*
@@ -19,6 +20,8 @@ public class DBConnect {
     public static Connection getConnection() {
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:tasks.db");
+            Statement stmt = conn.createStatement();
+            stmt.execute("CREATE TABLE IF NOT EXISTS \"tasks\" (\"task\" TEXT)");
             return conn;
         } catch (SQLException se) {
             JOptionPane.showMessageDialog(null, "Connection to the Database Failed!!", "Database Connection", JOptionPane.ERROR_MESSAGE);
