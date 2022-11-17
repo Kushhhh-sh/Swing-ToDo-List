2ff26c70496f914f5b3ab9f0d339a0f8e2a50dce
Kush Agarwal <agarwalkushajay@gmail.com>
2022-11-17 22:55:15 +0530

    Added Comments

--- src/task/TaskList.java
+++ src/task/TaskList.java
@@ -17,12 +17,19 @@ public class TaskList {
         taskList = new ArrayList<>();
     }
     
+    /**
+     * Prints all the tasks line-by-line that are available in the taskList
+     */
     public static void printAllTasks() {
         for (String next : taskList) {
             System.out.println(next);   
         }
     }
     
+    /**
+     * Returns a 2 Dimensional String array of all the tasks so that they can be displayed in the JTable
+     * @return 2 Dimensional String array
+     */
     public static String[][] get2DArray() {
         int size = taskList.size();
         String[][] array = new String[size][];
@@ -34,7 +41,21 @@ public class TaskList {
         return array;
     }
     
+    /**
+     * Adds a new task to the taskList ArrayList
+     * @param task String task entered by the user
+     * @return true if the task is added successfully else false
+     */
     public static boolean addTask(String task) {
         return taskList.add(task);
+    }
+    
+    /**
+     * Removes the Specified task from the taskList ArrayList
+     * @param task String task that the user Specifies to remove
+     * @return true if the task is removed successfully else false
+     */
+    public static boolean removeTask(String task) {
+        return taskList.remove(task);
     }
 }
